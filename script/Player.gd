9866491bbe3b929b50657a632a99ffc498c3b5ad
NixLon <74946055+NixLon@users.noreply.github.com>
2020-11-26 00:46:32 +0800

    Add files via upload
    - Added player animations based in "AnimationTree" functions.
    - Clean-up text.

--- script/Player.gd
+++ script/Player.gd
@@ -1,9 +1,13 @@
 extends KinematicBody2D
 
 const ACCELERATION = 800
-const MAX_SPEED = 30
+const MAX_SPEED = 50
 const FRICTION = 500
 var velocity = Vector2.ZERO
+
+onready var animationPlayer = $AnimationPlayer
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
 
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
@@ -13,9 +17,13 @@ func _physics_process(delta):
 
 	
 	if input_vector != Vector2.ZERO:
+		animationTree.set("parameters/IdleAnim/blend_position", input_vector)
+		animationTree.set("parameters/WalkAnim/blend_position", input_vector)
+		animationState.travel("WalkAnim")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	
 	else:
+		animationState.travel("IdleAnim")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 		
 	move_and_slide(velocity)
